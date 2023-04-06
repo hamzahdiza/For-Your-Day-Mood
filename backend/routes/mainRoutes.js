7ae17556b9fdc3d1caab4639f38854c17b25a75d
Hamzah Diza Santoso <hamzah.diza@gmail.com>
2023-04-06 16:15:54 +0700

    add 2

--- backend/routes/mainRoutes.js
+++ backend/routes/mainRoutes.js
@@ -7,5 +7,6 @@ router.use(authentication);
 router.post("/check-weather", mainController.generateWeather);
 router.post("/get-answer-ai", mainController.generateAnswer);
 router.post("/get-playlist", mainController.getPlaylist);
+router.post("/get-tracks");
 
 module.exports = router;
